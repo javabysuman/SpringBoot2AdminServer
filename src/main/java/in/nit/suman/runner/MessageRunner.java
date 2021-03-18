e644274d955aafed292eb5f88944e497527ec8d9
sumannit <sumannit636@gmail.com>
2021-03-18 22:49:54 +0530

    WH-7754 Log Updating

--- src/main/java/in/nit/suman/runner/MessageRunner.java
+++ src/main/java/in/nit/suman/runner/MessageRunner.java
@@ -1,12 +1,25 @@
 package in.nit.suman.runner;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+
+
 
 public class MessageRunner implements CommandLineRunner {
 
+	private static Logger log=LoggerFactory.getLogger(MessageRunner.class); 
+	
 	@Override
 	public void run(String... args) throws Exception {
 
+		try {
+			log.info("Started");
+		} catch (Exception e) {
+         e.printStackTrace();
+		}
+		log.info("END");
 		int a=10;
 		System.out.println("Hello From runner"+a);
 	}
